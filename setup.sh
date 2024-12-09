7087dbc0ddcb37e26a95a5a1aa410c00b43affee
adilet2306 <adilet.abdraimov9@gmail.com>
2024-12-09 22:30:21 +0000

    added script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+sudo apt install unzip -y
+wget https://releases.hashicorp.com/terraform/1.10.1/terraform_1.10.1_linux_amd64.zip
+unzip terraform_1.10.1_linux_amd64.zip
+sudo mv terraform /usr/local/bin
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo mv kubectl /usr/local/bin
+
+wget https://get.helm.sh/helm-v3.16.3-linux-amd64.tar.gz
+tar zxf helm-v3.16.3-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin
+
+rm -rf helm-v3.16.3-linux-amd64.tar.gz linux-amd64 terraform_1.10.1_linux_amd64.zip
