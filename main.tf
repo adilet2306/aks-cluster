e6e406241a1c63c91e1c1dea353bbd59d37660c7
adilet2306 <adilet.abdraimov9@gmail.com>
2024-12-09 23:05:05 +0000

    fixed size

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name       = "default"
     node_count = var.node_count
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "standard_d2ads_v6"
     vnet_subnet_id = azurerm_subnet.main.id
   }
 
