6f4f68d54b873afae9806ec7a3fe2a4434f36bbe
adilet2306 <adilet.abdraimov9@gmail.com>
2024-12-11 20:19:59 +0000

    new change

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ unzip terraform_1.10.1_linux_amd64.zip
 sudo mv terraform /usr/local/bin
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
 sudo mv kubectl /usr/local/bin
 
 wget https://get.helm.sh/helm-v3.16.3-linux-amd64.tar.gz
